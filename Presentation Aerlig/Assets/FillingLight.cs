e10d12ba21513b2308d1cdcfae3a1f38f4f681b5
Unknown <Robin.seignolles@hotmail.fr>
2018-10-14 20:11:16 +0200

    Courage, poussons !

---
+++ Presentation Aerlig/Assets/FillingLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FillingLight : MonoBehaviour {
+
+    public UnityEvent clicAnim;
+    public UnityEvent triggerAnim;
+    [SerializeField] bool animDone;
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !animDone)
+        {
+            clicAnim.Invoke();
+            animDone = true;
+        }
+    }
+    public void startAnim()
+    {
+        triggerAnim.Invoke();
+    }
+}
---
+++ Presentation Aerlig/Assets/GivePriority.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GivePriority : MonoBehaviour
+{
+    public CinemachineVirtualCamera actualCam;
+    public CinemachineVirtualCamera nextCam;
+
+
+    void GivePrio()
+    {
+        actualCam.m_Priority -= 1;
+        nextCam.m_Priority += 1;
+    }
+}
