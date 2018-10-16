0339574469ecb11c1b85cd61127303ed403344ce
Unknown <Robin.seignolles@hotmail.fr>
2018-10-16 15:03:54 +0200

    Git à jour pour Léna <3

---
+++ Presentation Aerlig/Assets/ShakeOnce.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+using Cinemachine;
+
+public class ShakeOnce : MonoBehaviour {
+
+    public CameraShaker cameraShake;
+
+	void Shake()
+    {
+        cameraShake.ShakeOnce(0.5f, 0.5f, 0.5f, 0.5f);
+    }
+}
